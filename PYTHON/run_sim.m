3a9f88292f6ed3d3a879ef9d5702689741edfc21
mcaron633 <marcocaron11@gmail.com>
2017-02-09 20:04:36 -0500

    end of day, probles with timing and singularities

--- PYTHON/run_sim.m
+++ PYTHON/run_sim.m
@@ -1,17 +1,44 @@
 function out = run_sim(data_in, m, target_speed_m_s, Crr, Cd, area)
 %data format is : lat, long, altitude, distance, gradient
+
 g=9.81;
 rho_air = 1.225;
 travel_speed = target_speed_m_s;
 
 fprintf('\n running matlab code')
-input_table_x_values = data_in(:,4)
-input_table_grad_values = data_in(:,5);
+input_table_x_values = data_in(1:900,4)';
+input_table_x_values = input_table_x_values - input_table_x_values(1)
+input_table_grad_values = data_in(1:900,5)';
 
-sim_out = sim('system_sim.slx');
 
-out = [0,1];
+options = simset('SrcWorkspace','current');
+s=sim('system_sim_test1.slx',[],options);
 
-disp(input_table_x_values)
+out = [0,1]  %simout;
+out = simout
+
+%disp(input_table_x_values)
+
+
+figure
+plot(s,out)
+figure
+plot(s,ramp)
+
+figure
+plot(input_table_x_values)
+
+figure
+plot(input_table_grad_values)
+
+figure
+plot(kout)
+
+
+figure
+plot(fout)
+
+figure
+plot(x_pos)
 
 end
