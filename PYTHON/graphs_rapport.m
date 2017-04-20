686546f8b4cf8dfd7317b013898ca780103a8703
mcaron633 <marcocaron11@gmail.com>
2017-04-20 12:57:12 -0400

    final final

--- PYTHON/graphs_rapport.m
+++ PYTHON/graphs_rapport.m
@@ -1,3 +1,9 @@
+load('wrkspc_no_13.mat')
+
+options = simset('SrcWorkspace','current');
+s = sim('system_sim',[],options);
+
+
 close all
 plot(pos_out/1000,p_out_positive)
 title('Puissance entree en fonction de la position')
@@ -22,4 +28,27 @@ title('Vitesse en fonction de la position')
 xlabel('position (km)')
 ylabel('Vitesse (km/h)')
 
+figure()
+plot(pos_out/1000,current_in)
+title('Courant en fonction de la position')
+xlabel('position (km)')
+ylabel('Courant (Amperes)')
+
+figure()
+plot(pos_out/1000,inst_spent_energy)
+title('Energie en fonction de la position')
+xlabel('position (km)')
+ylabel('Energie (J)')
+
+figure()
+plot(pos_out/1000,charge_in_a_h)
+title('Charge en fonction de la position')
+xlabel('position (km)')
+ylabel('Charge A-h')
+
+figure()
+plot(pos_out/1000,avg_current)
+title('Courant moyen en fonction de la position')
+xlabel('position (km)')
+ylabel('Courant A')
 
